b435bab8c4123b369c6b6a12bb24ede053ac8714
Luis Limón <132353147+Llimon90@users.noreply.github.com>
2023-10-19 12:32:56 -0700

    SE CORRIGE CREACIÓN DE LISTA EN TABLA app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-alert ("USUARIO: admin PASSWORD: admin ");
-
+//Información para iniciar sesión como ADMINISTRADOR
+alert("USUARIO: admin PASSWORD: admin ");
+        
 var catalogoAutos = [];
 var autenticacion = false;
 
-
+//esta función se encarga de cargar los datos de local storage previamente cargados es el arreglo "catalogoAutos" para mostrarlos en la tabla una vez que se llame la función mostrarLista.
 function cargarDatosDesdeLocalStorage() {
     var autosGuardados = localStorage.getItem('catalogoAutos');
     if (autosGuardados) {
@@ -11,29 +12,24 @@ function cargarDatosDesdeLocalStorage() {
         mostrarLista();
     }
 }
-//Con esta función alamcenamos los elementos del arreglo en el LocalStorage
+//convertir  datos de arreglo en una cadena JSON
 function guardarDatosEnLocalStorage() {
     localStorage.setItem('catalogoAutos', JSON.stringify(catalogoAutos));
 }
-//auqnue no es seguro guardar datos de autentificación del lado del cliente, realizamos autentificación para mostrar panel de CURD.
+//bloque de autenticación
 function login() {
     var usuario = document.getElementById("usuario").value;
     var password = document.getElementById("password").value;
 
     if (usuario === "admin" && password === "admin") {
         autenticacion = true;
-
+  
+    // Mostrar la menu de administrador y tabla de autos solo después de la autenticación, oculta formulario de inicio de sesióm
         document.getElementById("loginContenedor").style.display = "none";
         document.getElementById("bienvenidaUsuario").style.display = "block";
         document.getElementById("usuarioRegistrado").textContent = usuario;
-
-//si autenticacion se cumple, agregamos valores al arreglo.
-        catalogoAutos = [
-            { marca: "Toyota", modelo: "Camry", anio: "2022" },
-            { marca: "Honda", modelo: "Civic", anio: "2021" },
-        ];
-//si el if se cumple mostramos contenedor oculto y llamamos la funcion que imprime los datos de LocalStorage
         document.getElementById("contenedorCrud").style.display = "block";
+        document.getElementById("contenedorAutos").style.display = "block";
         mostrarLista();
     } else {
         alert("Credenciales incorrectas");
@@ -41,7 +37,8 @@ function login() {
 
     cargarDatosDesdeLocalStorage();
 }
-//En este bloque aplicamos la C de la app CRUD
+
+//Función que crea nuevos elementos en el arreglo. CRUD (CREATE)
 function agregarAuto() {
     var marcaAuto = document.getElementById("marcaAuto").value;
     var modeloAuto = document.getElementById("modeloAuto").value;
@@ -54,35 +51,40 @@ function agregarAuto() {
     } else {
         alert("Completa todos los campos del formulario");
     }
-//llamamos la funcion si todos los campos están completados
+
     guardarDatosEnLocalStorage();
 }
-//En este bloque aplicamos la R de CRUD
+
+//Función que lee elementos en el arreglo. CRUD (READ) y agrega elementos a tabla en el DOM
 function mostrarLista() {
-    var contenedorAutos = document.getElementById("catalogoAutos");
-    contenedorAutos.innerHTML = "";
+    var tbody = document.querySelector("#tablaCatalogoAutos tbody");
+    tbody.innerHTML = "";
 
     catalogoAutos.forEach(function (carro, index) {
-        var carroLista = document.createElement("li");
-        carroLista.innerText = `${carro.marca} ${carro.modelo} (${carro.anio})`;
+        var fila = tbody.insertRow();
+        var celdaMarca = fila.insertCell(0);
+        var celdaModelo = fila.insertCell(1);
+        var celdaAnio = fila.insertCell(2);
+        var celdaEditar = fila.insertCell(3);
+        var celdaEliminar = fila.insertCell(4);
 
+        celdaMarca.innerText = carro.marca;
+        celdaModelo.innerText = carro.modelo;
+        celdaAnio.innerText = carro.anio;
+
+        // Asignamos funcionalidad a los botones editar y eliminar llamando a su función correspondiente
         var botonEditar = document.createElement("button");
         botonEditar.innerText = "Editar";
         botonEditar.onclick = function () { editarAuto(index); };
+        celdaEditar.appendChild(botonEditar);
 
         var botonEliminar = document.createElement("button");
         botonEliminar.innerText = "Eliminar";
         botonEliminar.onclick = function () { borrarAuto(index); };
-
-        carroLista.appendChild(botonEditar);
-        carroLista.appendChild(botonEliminar);
-
-        contenedorAutos.appendChild(carroLista);
+        celdaEliminar.appendChild(botonEliminar);
     });
-
-    guardarDatosEnLocalStorage();
 }
-//En este bloque aplicamos U de CRUD para actualizar elementos
+//Función que edita nuevos elementos en el arreglo. CRUD (UPDATE)
 function editarAuto(index) {
     var carro = catalogoAutos[index];
     var actualizarMarca = prompt("Editar Marca:", carro.marca);
@@ -100,18 +102,16 @@ function editarAuto(index) {
     mostrarLista();
     guardarDatosEnLocalStorage();
 }
-
-//En este bloque aplicamos D de CRUD para borrar elementos. 
+//Función que crea elimina elementos en el arreglo. CRUD (DELETE)
 function borrarAuto(index) {
     var confirmDelete = confirm("¿Estás seguro de que deseas eliminar este auto?");
 
     if (confirmDelete) {
         catalogoAutos.splice(index, 1);
         mostrarLista();
-        
         guardarDatosEnLocalStorage();
     }
 }
 
-        cargarDatosDesdeLocalStorage()
+cargarDatosDesdeLocalStorage();
 
